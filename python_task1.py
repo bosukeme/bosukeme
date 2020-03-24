474c191cbcee89ad83e11060cfff2d71d33ae77d
Ukeme Wilson <42305512+bosukeme@users.noreply.github.com>
2020-03-24 18:25:06 +0100

    Adding my python task 1 solution

---
+++ python_task1.py
@@ -0,0 +1,9 @@
+#formula is pi *r*r
+def calculate_area():
+    radius=int(input("What is the area of the circle?: "))
+    pi=2.143
+    formula= pi *radius*radius
+    print("The area of the circle with radius of", radius, "is eqaul to", formula)
+    
+calculate_area()
+    
